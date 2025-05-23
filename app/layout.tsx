d8af95a5ff398e2e3edd50a532a106d0b90e9a8f
N.I <iworksWeb@gmail.com>
2025-05-23 14:35:08 +0900

    npm run lint:fix #8

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,18 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
+  variable: "--font-geist-sans",
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
+  variable: "--font-geist-mono",
 });
 
 export const metadata: Metadata = {
-  title: "Novel Smith",
   description: "あなただけの本を作ろう",
+  title: "Novel Smith",
 };
 
 export default function RootLayout({
