7c4e08d38bff79de71517dfb417bc8c2ad1d5dc8
N.I <iworksWeb@gmail.com>
2025-06-15 20:18:46 +0900

    add app/api/[trpc]/route.ts #32

---
+++ app/api/[trpc]/route.ts
@@ -0,0 +1,12 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "@/trpc/server";
+
+const handler = (req: Request) =>
+  fetchRequestHandler({
+    createContext: () => ({}),
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+  });
+
+export { handler as GET, handler as POST };
