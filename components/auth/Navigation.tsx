c4e8e63aaae95f8d5597445d3d1139b415880bb0
N.I <iworksWeb@gmail.com>
2025-06-16 14:14:54 +0900

    fix ci typecheck to components/auth/Navigation.tsx #32

--- components/auth/Navigation.tsx
+++ components/auth/Navigation.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import { type Session } from "next-auth";
 
-const Navigation = () => {
+interface NavigationProps {
+  user: null | Session | undefined;
+}
+
+const Navigation = ({ user }: NavigationProps) => {
+  console.log(user);
+
   return (
     <header className="shadow-lg shadow-gray-100 mb-10">
-      <div className="container mx-auto flex max-w-screen-md items-center justify-between px-4">
+      <div className="container mx-auto flex max-w-screen-md items-center justify-between px-4 py-4">
         <Link className="cursor-pointer text-xl font-bold" href="/">
           Novel Smith
         </Link>
