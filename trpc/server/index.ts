38a75dcf74df05932cb2ad3b107cb1b72fb11c35
N.I <iworksWeb@gmail.com>
2025-06-22 00:52:38 +0900

    add trpc stroy route #40

--- trpc/server/index.ts
+++ trpc/server/index.ts
@@ -1,9 +1,11 @@
 import { router } from "./trpc";
 import { authRouter } from "@/trpc/server/routers/auth";
 import { userRouter } from "@/trpc/server/routers/user";
+import { storyRouter } from "@/trpc/server/routers/story";
 
 export const appRouter = router({
   auth: authRouter,
+  story: storyRouter,
   user: userRouter,
 });
 
